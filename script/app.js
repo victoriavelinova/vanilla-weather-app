29d51b09f414b838f64a97ed99bc19da789026fb
victoriavelinova <velinovavictoria@hotmail.de>
2021-11-04 14:46:00 +0100

    added the search engine

--- script/app.js
+++ script/app.js
@@ -63,8 +63,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "197ef3a642b76eef90e131866f74a0a0";
-let city = "Vienna";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "197ef3a642b76eef90e131866f74a0a0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Vienna");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
